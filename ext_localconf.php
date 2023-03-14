e6b34b1dcc7e2731f4d21106b99b3ffaa516f6dc
Torben Hansen <derhansen@gmail.com>
2023-03-14 08:55:51 +0100

    [TASK] Avoid usage of TYPO3_MODE constant

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,5 @@
 <?php
-defined('TYPO3_MODE') or die();
+defined('TYPO3') or die();
 
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ke_search']['registerIndexerConfiguration'][] =
     \Derhansen\SfEventMgtIndexer\Indexer\EventIndexer::class;
